3509c1ce376b79dd407d8c3add72f7d8fb9fb415
Cesar <u201710947@upc.edu.pe>
2019-09-25 23:04:23 -0500

    Ahora compatible con Java 1.8

--- UPCRideAndroid/app/src/main/java/com/example/upcride/activities/MainActivity.java
+++ UPCRideAndroid/app/src/main/java/com/example/upcride/activities/MainActivity.java
@@ -2,15 +2,27 @@ package com.example.upcride.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.upcride.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnIrRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIrRegistro = (Button) findViewById(R.id.btnIrRegistro);
+
+        btnIrRegistro.setOnClickListener((v) -> {
+            Intent intent = new Intent(getApplicationContext(), RegistroGeneral.class);
+            startActivity(intent);
+        });
+
+
     }
 }
