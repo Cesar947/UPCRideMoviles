0af8cd23f24bc6b625b08a8ff68dbb33243c58aa
bbqms <bbqms@hotmail.com>
2019-09-26 17:49:38 -0500

    clases kt

---
+++ UPCRideAndroid/app/src/main/java/com/example/upcride/model/Transaccion.kt
@@ -0,0 +1,3 @@
+package com.example.upcride.model
+
+class Transaccion(var id: Int?, var pasajero: Usuario?, var viaje: Viaje?)
---
+++ UPCRideKotlin/app/src/main/java/com/example/upcridekotlin/model/Transaccion.kt
@@ -0,0 +1,3 @@
+package com.example.upcridekotlin.model
+
+class Transaccion(var id: Int?, var pasajero: Usuario?, var viaje: Viaje?)
