ea325e23f5d70d4eeabeb4674d775488593acb77
Bryan <bbqms@hotmail.com>
2019-09-27 07:24:46 -0500

    interfaces

--- UPCRideKotlin/app/src/main/java/com/example/upcridekotlin/interfaces/SolicitudApiService.kt
+++ UPCRideKotlin/app/src/main/java/com/example/upcridekotlin/interfaces/SolicitudApiService.kt
@@ -1,4 +1,16 @@
 package com.example.upcridekotlin.interfaces
 
+import com.example.upcridekotlin.model.Auto
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface SolicitudApiService {
+
+    @GET("autos")
+    fun getAllSolicitudes(): Call<List<Auto>>
+
+    @POST("autos")
+    fun insetarSolicitud(@Body auto: Auto?): Call<Auto>
 }
--- UPCRideKotlin/app/src/main/java/com/example/upcridekotlin/interfaces/TransaccionApiService.kt
+++ UPCRideKotlin/app/src/main/java/com/example/upcridekotlin/interfaces/TransaccionApiService.kt
@@ -1,4 +1,17 @@
 package com.example.upcridekotlin.interfaces
 
+import com.example.upcridekotlin.model.Auto
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface TransaccionApiService {
+
+    @GET("autos")
+    fun getAllTransacciones(): Call<List<Auto>>
+
+    @POST("autos")
+    fun insertarTransaccion(@Body auto: Auto?): Call<Auto>
 }
+
