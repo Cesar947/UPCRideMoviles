4ac82351b593d9f5ff7ce0443bf93b0a0cfb4282
Cesar Pizarro Llanos <cesarpizarrollanos@Cesars-Mac.local>
2019-11-04 08:59:58 -0800

    clases del model ocreadas, pero falta

---
+++ UPCRideiOS/UPCRideiOS/Models/Ride.swift
@@ -0,0 +1,30 @@
+//
+//  Ride.swift
+//  UPCRideiOS
+//
+//  Created by Cesar Pizarro Llanos on 11/4/19.
+//  Copyright © 2019 UPC Ride. All rights reserved.
+//
+
+import Foundation
+//FALTA
+
+struct Viaje {
+    var id: Int
+    var codigo: Int
+    var dni: Int
+    var correoUpc: String
+    var contrasena: String
+    var nombres: String
+    var apellidos: String
+    var facebookId: String
+    var telefono: String
+    var rol: Character
+    var licenciaConducitr: String
+    var sede: String
+    var distrito: String
+    var ubicacionLatitud: String
+    var ubicacionLongitud: String
+
+}
+
