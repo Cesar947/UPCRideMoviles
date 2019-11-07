0b4cff12c24dab5a12e30b755f735f508b720d3d
Sebastian <sebastian@Sebastians-Mac.local>
2019-11-06 20:22:49 -0800

    CardView Terminada

--- UPCRideiOS/UPCRideiOS/View/ViajeCardView.swift
+++ UPCRideiOS/UPCRideiOS/View/ViajeCardView.swift
@@ -15,55 +15,77 @@ struct ViajeCardView: View {
             Image("loli")
                   .resizable()
                    .aspectRatio(contentMode: .fill)
-                  .frame(width: 50, height: 50)
+                  .frame(width: 30, height: 30)
                   .clipped()
                 .cornerRadius(50)
             VStack {
                 Text("Brian Miramira")
-                    .font(.body)
+                    .font(.caption)
                     .fontWeight(.semibold)
                     .multilineTextAlignment(.center)
-                    .padding([.bottom, .trailing])
+                    .padding(.trailing, 30)
+                .padding(.bottom, 5)
                 
                 
                 Text("09/11/2019 08:00 pm")
-                    .font(.body)
+                    .font(.caption)
                     .fontWeight(.thin)
                     .lineLimit(2)
                     .multilineTextAlignment(.center)
+                    .padding(.leading, 3.0)
                 
-            }.padding()
+            }.padding(.trailing, 160)
             
-        }
+        }.padding(.top, 20)
           
         Text("Viaje de San Martin de Miradflores a San Juan de Burga")
-            .padding(.all)
+            .font(.callout)
+            .padding(.top, 10)
+            .padding([.trailing, .leading], 24.5)
         
         HStack {
             Image("left")
-                .padding(.trailing)
+                .padding(.trailing, 5)
+
             VStack {
                 Text("Casita")
-                    .padding(.bottom)
+                    .padding(.top, 10)
+                    .padding(.trailing, 155)
+                Image("divider")
                 Text("UPCex")
+                    .padding(.top, 5)
+                    .padding(.trailing, 155)
             }
             .padding(.bottom)
-        }
+        }.padding(.trailing, 50)
             
           
-          Button(action: {
-              print("Tap tap tap...")
-          }) {
-              Text("Solicitar")
-                  .foregroundColor(Color.black)
-                  .padding()
-                  .background(Color(red: 1.0, green: 0.6, blue: 0, opacity: 0.6))
-                  .cornerRadius(12)
-          }.padding()
+        HStack {
+            Button(action: {
+                  print("Viaje Comentado")
+              }) {
+                  Text(" ")
+                      .foregroundColor(Color.black)
+                    .padding(.bottom, 2)
+                    .padding(.trailing, 50)
+                      .background(Image("icons_comment"))
+            }.padding(5)
+            
+            Button(action: {
+                  print("Viaje Solicitado")
+              }) {
+                  Text(" ")
+                      .foregroundColor(Color.black)
+                    .padding(.bottom, 2)
+                      .background(Image("icons_rideshare"))
+            }.padding(5)
+        }.padding(.leading, 220)
       }
       .padding()
       .background(Color.white)
       .cornerRadius(28)
+      .frame(width: 390, height: 100)
+      .shadow(radius: 8, y: 14)
     }
 }
 
